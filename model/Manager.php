78d99f3a68c50c1d2883b249c6fabf6b8060ed36
Eric Codron <eric.codron@gmail.com>
2018-01-25 17:40:53 +0100

    Minor bug fix in Manager.php.

--- model/Manager.php
+++ model/Manager.php
@@ -6,7 +6,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        require_once("config.php");
+        include("config.php");
 
         $db = new \PDO('mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8', ''.$db_user.'', ''.$db_pass.'');
         return $db;
