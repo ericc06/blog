f82008b0a394d3d1d1f7f7410374382e644bd49a
Eric Codron <eric.codron@gmail.com>
2018-02-03 07:35:10 +0100

    Minor bug fix (JS alert removed).

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -1,5 +1,4 @@
 $(function() {
-alert("test");
     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
